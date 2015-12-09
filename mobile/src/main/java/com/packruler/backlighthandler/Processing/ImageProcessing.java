3a2dbcdd46e259b34d17d3d24d34b0102e53389d
packruler <packruler@gmail.com>
2015-12-09 12:34:17 -0500

    Native code not working properly

--- mobile/src/main/java/com/packruler/backlighthandler/Processing/ImageProcessing.java
+++ mobile/src/main/java/com/packruler/backlighthandler/Processing/ImageProcessing.java
@@ -1,6 +1,7 @@
 package com.packruler.backlighthandler.Processing;
 
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.media.Image;
 import android.util.Log;
 
@@ -21,8 +22,7 @@ public class ImageProcessing {
     private int[] vR, vG, vB;
     private int hSpacing, vSpacing;
 
-    private native int[] processNative(int[] buffer);
-
+    private native void processNative(Bitmap bitmap, int[] horizontalLED, int[] verticalLED, int xDepth, int yDepth);
 
     public ImageProcessing() {
         xDepth = 20;
@@ -51,68 +51,61 @@ public class ImageProcessing {
 
     public void process(Image image) {
         Image.Plane[] planes = image.getPlanes();
-        ByteBuffer byteBuffer = planes[0].getBuffer();
-        int[] pixelArray = planes[0].getBuffer().asIntBuffer().array();
-        int iHeight = image.getHeight();
-        int iWidth = image.getWidth();
         Bitmap bitmap = Bitmap.createBitmap(image.getWidth(), image.getHeight(), Bitmap.Config.RGB_565);
         bitmap.copyPixelsFromBuffer(planes[0].getBuffer());
-        int x = 0;
-        int y = 0;
-        StringBuffer buffer = new StringBuffer("Pixels:\n");
-//        while (byteBuffer.)
-        for (int pixel = 0; pixel < pixelArray.length; pixel++) {
-            if (x == iWidth) {
-                x = 0;
-                y++;
-                buffer.append("\n");
-            }
-            x++;
-            buffer.append('(').append(Integer.toHexString(x)).append(',').append(Integer.toHexString(y)).append(')');
-        }
-        Log.i(TAG, buffer.toString());
 
-//        int yOffset = -1;
-//        int xOffset = -1;
-//        int pixel;
-//        for (int y = 0; y < iHeight / 2; y++) {
-//            for (int x = 0; x < iWidth / 2; x++) {
-//                pixel = pixelArray[(y*iWidth)+iWidth]
-//                if (Color.red(pixel) > 0 ||
-//                        Color.green(pixel) > 0 ||
-//                        Color.blue(pixel) > 0) {
-//                    yOffset = y;
-//                    xOffset = x;
-//                    break;
-//                }
-//            }
-//            if (yOffset != -1 && xOffset != -1)
-//                break;
-//        }
-//        if (yOffset < 0)
-//            yOffset = 0;
-//        if (xOffset < 0)
-//            xOffset = 0;
-//
-//        StringBuffer buffer = new StringBuffer("Colors: ");
-//        int currentR, currentG, currentB;
-//        for (int y = yOffset; y < bitmap.getHeight() - yOffset; y++) {
-//            for (int x = xOffset; x < bitmap.getWidth() - xOffset; x++) {
-//                pixel = bitmap.getPixel(x, bitmap.getHeight() / 2);
-//                currentR = Color.red(pixel);
-//                currentG = Color.green(pixel);
-//                currentB = Color.blue(pixel);
-//
-//                if (!(y < yOffset + yDepth &&
-//                        y > bitmap.getHeight() - yOffset - yDepth) &&
-//                        x > xOffset + xDepth && x < bitmap.getWidth() - xOffset - xDepth)
-//                    x = bitmap.getWidth() - xOffset - xDepth;
-//            }
-//        }
-//        buffer.append("\n");
-//
-//
-//        image.close();
+
+        int[] pixelArray = new int[bitmap.getHeight() * bitmap.getWidth()];
+        bitmap.getPixels(pixelArray, 0, 0, 0, 0, bitmap.getWidth(), bitmap.getHeight());
+
+        ByteBuffer byteBuffer = planes[0].getBuffer();
+        Log.i(TAG, "Position: " + byteBuffer.position());
+        byte[] bytes = new byte[image.getHeight() * image.getWidth() * 3];
+        byteBuffer.position(0);
+        byteBuffer.get(bytes);
+        Log.i(TAG, "Size: " + bytes.length);
+
+        int yOffset = -1;
+        int xOffset = -1;
+        int pixel;
+        for (int y = 0; y < bitmap.getHeight() / 2; y++) {
+            for (int x = 0; x < bitmap.getWidth() / 2; x++) {
+                pixel = bitmap.getPixel(x, y);
+                if (Color.red(pixel) > 0 ||
+                        Color.green(pixel) > 0 ||
+                        Color.blue(pixel) > 0) {
+                    yOffset = y;
+                    xOffset = x;
+                    break;
+                }
+            }
+            if (yOffset != -1 && xOffset != -1)
+                break;
+        }
+        if (yOffset < 0)
+            yOffset = 0;
+        if (xOffset < 0)
+            xOffset = 0;
+
+        StringBuffer buffer = new StringBuffer("Colors: ");
+        int currentR, currentG, currentB;
+        for (int y = yOffset; y < bitmap.getHeight() - yOffset; y++) {
+            for (int x = xOffset; x < bitmap.getWidth() - xOffset; x++) {
+                pixel = bitmap.getPixel(x, bitmap.getHeight() / 2);
+                currentR = Color.red(pixel);
+                currentG = Color.green(pixel);
+                currentB = Color.blue(pixel);
+
+                if (!(y < yOffset + yDepth &&
+                        y > bitmap.getHeight() - yOffset - yDepth) &&
+                        x > xOffset + xDepth && x < bitmap.getWidth() - xOffset - xDepth)
+                    x = bitmap.getWidth() - xOffset - xDepth;
+            }
+        }
+        buffer.append("\n");
+
+
+        image.close();
     }
 
     private Bitmap removeBlackBars(Image image) {
--- mobile/src/main/jni/imageprocessing.c
+++ mobile/src/main/jni/imageprocessing.c
@@ -1,12 +1,33 @@
 #include <jni.h>
+#include <jni.h>
+#include <android/log.h>
+#include <stdio.h>
+#include <android/bitmap.h>
+#include <string.h>
+#include <unistd.h>
 
-JNIEXPORT jintArray JNICALL
+#define  LOG_TAG    "DEBUG"
+#define  LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG,LOG_TAG,__VA_ARGS__)
+#define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
+
+JNIEXPORT void JNICALL
 Java_com_packruler_backlighthandler_Processing_ImageProcessing_processNative(JNIEnv *env,
                                                                              jobject instance,
-                                                                             jintArray buffer_) {
-    jint *buffer = (*env)->GetIntArrayElements(env, buffer_, NULL);
+                                                                             jobject bitmap,
+                                                                             jintArray horizontalLED_,
+                                                                             jintArray verticalLED_,
+                                                                             jint xDepth,
+                                                                             jint yDepth) {
+    jint *horizontalLED = (*env)->GetIntArrayElements(env, horizontalLED_, NULL);
+    jint *verticalLED = (*env)->GetIntArrayElements(env, verticalLED_, NULL);
 
+    AndroidBitmapInfo *info;
+    void **pixels;
+
+    AndroidBitmap_getInfo(env, bitmap, info);
+    AndroidBitmap_lockPixels(env, bitmap, pixels);
     // TODO
 
-    (*env)->ReleaseIntArrayElements(env, buffer_, buffer, 0);
+    (*env)->ReleaseIntArrayElements(env, horizontalLED_, horizontalLED, 0);
+    (*env)->ReleaseIntArrayElements(env, verticalLED_, verticalLED, 0);
 }
